fd6eac15dcfcbaaf6e7e10ae5ddfa6401fc57a8d
Nadun Nissanka <61065783+Nadunnissanka@users.noreply.github.com>
2021-07-21 20:49:01 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import pandas
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.setup(725, 491)
+screen.addshape(image)
+turtle = Turtle()
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+count = 0
+guessed_states = []
+game_is_on = True
+
+while game_is_on:
+    answer_state = screen.textinput(title=f"{count}/50 Guess the state", prompt="What's another state's name?").title()
+    if count >= 50:
+        game_is_on = False
+    elif answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+            data_dict = {
+                "missing state name": missing_states
+            }
+            df = pandas.DataFrame(data_dict)
+            df.to_csv("learn.csv")
+        game_is_on = False
+    else:
+        if answer_state in all_states:
+            guessed_states.append(answer_state)
+            t = Turtle()
+            t.hideturtle()
+            t.penup()
+            selected_state = data[data.state == answer_state]
+            t.goto(int(selected_state.x), int(selected_state.y))
+            t.write(answer_state)
+            count += 1
+
+# states to learn.csv
+
+screen.exitonclick()
+
+
+
